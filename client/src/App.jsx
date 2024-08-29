7113bb6fd18dc05fb956c510f321a16f25ae3216
erazo <erazorodriguezjuanesteban@gmail.com>
2024-08-29 18:26:35 -0500

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import About from './pages/About'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/profile' element={<Profile/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/sign-in' element={<Signin/>} />
-        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
